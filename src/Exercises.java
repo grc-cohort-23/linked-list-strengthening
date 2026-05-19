89213580857ca0d78afc399a8f9a600bab3b9ffd
onjamm <75151498+onjamm@users.noreply.github.com>
2026-05-18 19:08:11 -0700

    finished isDoubled

--- src/Exercises.java
+++ src/Exercises.java
@@ -165,7 +165,20 @@ public class Exercises {
      * @return whether the values in bigList are twice the values in smallList
      */
     public static boolean isDoubled(ListNode smallList, ListNode bigList) {
-        return false;
+
+        if (length(smallList) != length(bigList)) {
+            return false;
+        }
+        while (smallList != null && bigList != null) {
+            if (bigList.data != smallList.data * 2) {
+                return false;
+            }
+
+            bigList = bigList.next;
+            smallList = smallList.next;
+        }
+
+        return true;
     }
 
     /**
